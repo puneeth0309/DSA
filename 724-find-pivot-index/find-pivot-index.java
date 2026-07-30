bdc585691d065b287b23bc2667d3421de32646bb
Puneeth <kottisapuneeth09@gmail.com>
2026-07-30 11:38:27 +0530

    Time: 1 ms (97.52%) | Memory: 47.5 MB (41.05%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+          int left=0;
+          int total=0;
+          for(int i=0;i<nums.length;i++){
+            total+=nums[i];
+          }
+          for(int i=0;i<nums.length;i++){
+            int right=total-left-nums[i];
+            if(right==left) return i;
+            left+=nums[i];
+          }
+          return -1;
+    }
+}
